81d3f8dcfe81e897a6a01868eec8a2644cc7acb3
anourethpongrattana <anoureth.pongrattana@gmail.com>
2023-11-18 15:03:54 +0100

    finalize lab

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,3 +1,89 @@
-function CountryDetails() {}
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Navbar from "../components/Navbar";
+import { Link, useParams } from "react-router-dom";
+
+function CountryDetails() {
+  const { countryId } = useParams();
+  const [countriesDetails, setCountriesDetails] = useState();
+  useEffect(() => {
+    axios
+      .get(`https://ih-countries-api.herokuapp.com/countries/${countryId}`)
+      .then((res) => {
+        console.log(res.data);
+        setCountriesDetails(res.data);
+      })
+      .catch((error) => console.error(error));
+    console.log(countryId);
+  }, [countryId]);
+
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <p style={{ fontSize: "24px", fontWeight: "bold" }}>Country Details</p>
+        {!countriesDetails ? <p>Data is Loading...</p> : (
+          <>
+          <img  src={`https://flagpedia.net/data/flags/icon/72x54/${countriesDetails.alpha2Code.toLowerCase()}.png`} alt="" />
+            <h1>{countriesDetails.name.common}</h1>
+
+            <table className="table">
+              <thead></thead>
+              <tbody>
+                <tr>
+                  <td style={{ width: "30%" }}>Capital</td>
+                  <td>{countriesDetails.capital[0]}</td>
+                </tr>
+                <tr>
+                  <td>Area</td>
+                  <td>
+                    {countriesDetails.area} km
+                    <sup>2</sup>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Borders</td>
+                  <td>
+                    <ul>
+                      {countriesDetails.borders.map((border, index) => (
+                         <li key={index}>
+                          <Link to={`/${border}`}>{border}</Link>
+                        </li>
+                      ))}
+                      {/* <li>
+                        <a href="/AND">Andorra</a>
+                      </li>
+                      <li>
+                        <a href="/BEL">Belgium</a>
+                      </li>
+                      <li>
+                        <a href="/DEU">Germany</a>
+                      </li>
+                      <li>
+                        <a href="/ITA">Italy</a>
+                      </li>
+                      <li>
+                        <a href="/LUX">Luxembourg</a>
+                      </li>
+                      <li>
+                        <a href="/MCO">Monaco</a>
+                      </li>
+                      <li>
+                        <a href="/ESP">Spain</a>
+                      </li>
+                      <li>
+                        <a href="/CHE">Switzerland</a>
+                      </li> */}
+                    </ul>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </>
+        )}
+      </div>
+    </>
+  );
+}
 
 export default CountryDetails;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,69 @@
-function HomePage() {}
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Navbar from "../components/Navbar";
+import { Link } from "react-router-dom";
+
+function HomePage() {
+  const [countries, setCountries] = useState([]);
+  const countriesAPI = "https://ih-countries-api.herokuapp.com/countries";
+
+  async function fetchCountries() {
+    // With fetch
+    // try {
+    //   const response = await fetch(countriesAPI);
+    //   const data = await response.json();
+    //   setCountries(data);
+    // } catch (error) {
+    //   console.log(error);
+    // }
+
+    // With axios
+    axios
+      .get(`${countriesAPI}`)
+      .then((response) => {
+        console.log(response);
+        setCountries(response.data);
+      })
+      .catch((error) => console.log(error));
+  }
+
+  useEffect(() => {
+    fetchCountries();
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      <div
+        className="container"
+        style={{ maxHeight: "90vh", overflow: "scroll" }}
+      >
+        <h1 style={{ fontSize: "24px" }}>
+          WikiCountries: Your Guide to the World
+        </h1>
+
+        <div className="list-group">
+          {countries &&
+            countries.map((country) => {
+              return (
+                <Link
+                  //   style={{ display: "flex", flexDirection: "column", alignItems:"center" }}
+                  className="list-group-item list-group-item-action"
+                  key={country._id}
+                  to={`/${country.alpha3Code}`}
+                >
+                  <img
+                    //   style={{height:"2rem", width:"2rem"}}
+                    src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`}
+                  />
+                  <div>{country.name.common}</div>
+                </Link>
+              );
+            })}
+        </div>
+      </div>
+    </>
+  );
+}
 
 export default HomePage;
